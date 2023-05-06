989cd11e0d91be5dba222bafb11f88d5d9bd3677
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-06 00:40:47 -0700

    new

--- questions.py
+++ questions.py
@@ -1,2 +1,2 @@
 #this will store as the question and answer bank for vim commands at my current level
-bank = {"What is the command for going right" : 'l', "What is the command for going down": 'j','What is the command for going left' : 'h', 'What is the command for going up': 'k', 'What is the command for going to end of line and insert mode': 'ctrl + a']
+bank = {"What is the command for going right" : 'l', "What is the command for going down": 'j','What is the command for going left' : 'h', 'What is the command for going up': 'k', 'What is the command for going to end of line and insert mode': 'ctrl + a','What is the command to delete current word and go into insert mode': 'ciw' ]
