99d5484c48907740606780b901b83af0657caa8c
Randy Do <randyydoo@csu.fullerton.edu>
2023-05-08 18:54:52 -0700

    updated

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-import random from random
+import questions
+import random 
 def main():
+    temp = []
+    score = 0
+    while True:
+        question = random.choice([questions.bank.keys()])
+        while question in temp:
+            question = random.choice([questions.bank.keys()])
+        answer = questions.bank[question]
+        print(f"Current score: {score} \n -------------------\n")
+        ans = input(question, "Your answer(type 'exit' to exit': ")
+        if ans is not answer:
+            score = 0
+            temp = []
+            continue
+        elif ans == "exit":
+            break
+        score += 1
+        temp.append(question)
+main()
